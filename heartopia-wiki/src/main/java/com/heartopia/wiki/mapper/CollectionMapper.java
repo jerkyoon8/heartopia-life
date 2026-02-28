f89fc8ca9435fcdad82707fe0e28f1377c63eb03
juwon1 <khazan098@naver.com>
2026-02-28 11:43:31 +0900

    feat: 아이템 상세보기 페이지 구현

--- heartopia-wiki/src/main/java/com/heartopia/wiki/mapper/CollectionMapper.java
+++ heartopia-wiki/src/main/java/com/heartopia/wiki/mapper/CollectionMapper.java
@@ -1,22 +1,25 @@
 package com.heartopia.wiki.mapper;
 
 import com.heartopia.wiki.model.AnimalCollection;
+import com.heartopia.wiki.model.BirdCollection;
+import com.heartopia.wiki.model.BugCollection;
 import com.heartopia.wiki.model.CookingCollection;
 import com.heartopia.wiki.model.FishCollection;
 import com.heartopia.wiki.model.FlowerCollection;
 import com.heartopia.wiki.model.GardeningCollection;
 import com.heartopia.wiki.model.ForageableCollection;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 @Mapper
 public interface CollectionMapper {
+    // === 전체 조회 ===
     List<FishCollection> findAllFish();
 
-    // Using BirdCollection and BugCollection
-    List<com.heartopia.wiki.model.BirdCollection> findAllBirds();
+    List<BirdCollection> findAllBirds();
 
-    List<com.heartopia.wiki.model.BugCollection> findAllBugs();
+    List<BugCollection> findAllBugs();
 
     List<AnimalCollection> findAllAnimals();
 
@@ -28,7 +31,57 @@ public interface CollectionMapper {
 
     List<ForageableCollection> findAllForageables();
 
-    // Count methods
+    // === 이름으로 개별 조회 (상세보기) ===
+    FishCollection findFishByName(@Param("name") String name);
+
+    BugCollection findBugByName(@Param("name") String name);
+
+    BirdCollection findBirdByName(@Param("name") String name);
+
+    AnimalCollection findAnimalByName(@Param("name") String name);
+
+    CookingCollection findCookingByName(@Param("name") String name);
+
+    FlowerCollection findFlowerByName(@Param("name") String name);
+
+    GardeningCollection findCropByName(@Param("name") String name);
+
+    ForageableCollection findForageableByName(@Param("name") String name);
+
+    // === 같은 장소 관련 아이템 조회 (관련정보) ===
+    List<FishCollection> findFishByLocation(@Param("location") String location,
+            @Param("excludeName") String excludeName);
+
+    List<BugCollection> findBugsByByLocation(@Param("location") String location,
+            @Param("excludeName") String excludeName);
+
+    List<BirdCollection> findBirdsByLocation(@Param("location") String location,
+            @Param("excludeName") String excludeName);
+
+    List<AnimalCollection> findAnimalsByLocation(@Param("location") String location,
+            @Param("excludeName") String excludeName);
+
+    List<ForageableCollection> findForageablesByLocation(@Param("location") String location,
+            @Param("excludeName") String excludeName);
+
+    // === 키워드 검색 ===
+    List<FishCollection> searchFish(@Param("keyword") String keyword);
+
+    List<BugCollection> searchBugs(@Param("keyword") String keyword);
+
+    List<BirdCollection> searchBirds(@Param("keyword") String keyword);
+
+    List<AnimalCollection> searchAnimals(@Param("keyword") String keyword);
+
+    List<CookingCollection> searchCookings(@Param("keyword") String keyword);
+
+    List<FlowerCollection> searchFlowers(@Param("keyword") String keyword);
+
+    List<GardeningCollection> searchCrops(@Param("keyword") String keyword);
+
+    List<ForageableCollection> searchForageables(@Param("keyword") String keyword);
+
+    // === Count ===
     int countAllFish();
 
     int countAllBirds();
--- heartopia-wiki/src/main/java/com/heartopia/wiki/service/CollectionService.java
+++ heartopia-wiki/src/main/java/com/heartopia/wiki/service/CollectionService.java
@@ -20,6 +20,7 @@ public class CollectionService {
 
     private final CollectionMapper collectionMapper;
 
+    // === 전체 조회 (기존) ===
     public List<FishCollection> getAllFish() {
         return collectionMapper.findAllFish();
     }
@@ -52,7 +53,94 @@ public class CollectionService {
         return collectionMapper.findAllForageables();
     }
 
-    // Connect to Mapper Count methods
+    // === 이름으로 개별 조회 (상세보기) ===
+    public FishCollection getFishByName(String name) {
+        return collectionMapper.findFishByName(name);
+    }
+
+    public BugCollection getBugByName(String name) {
+        return collectionMapper.findBugByName(name);
+    }
+
+    public BirdCollection getBirdByName(String name) {
+        return collectionMapper.findBirdByName(name);
+    }
+
+    public AnimalCollection getAnimalByName(String name) {
+        return collectionMapper.findAnimalByName(name);
+    }
+
+    public CookingCollection getCookingByName(String name) {
+        return collectionMapper.findCookingByName(name);
+    }
+
+    public FlowerCollection getFlowerByName(String name) {
+        return collectionMapper.findFlowerByName(name);
+    }
+
+    public GardeningCollection getCropByName(String name) {
+        return collectionMapper.findCropByName(name);
+    }
+
+    public ForageableCollection getForageableByName(String name) {
+        return collectionMapper.findForageableByName(name);
+    }
+
+    // === 같은 장소 관련 아이템 조회 (관련정보) ===
+    public List<FishCollection> getFishByLocation(String location, String excludeName) {
+        return collectionMapper.findFishByLocation(location, excludeName);
+    }
+
+    public List<BugCollection> getBugsByLocation(String location, String excludeName) {
+        return collectionMapper.findBugsByByLocation(location, excludeName);
+    }
+
+    public List<BirdCollection> getBirdsByLocation(String location, String excludeName) {
+        return collectionMapper.findBirdsByLocation(location, excludeName);
+    }
+
+    public List<AnimalCollection> getAnimalsByLocation(String location, String excludeName) {
+        return collectionMapper.findAnimalsByLocation(location, excludeName);
+    }
+
+    public List<ForageableCollection> getForageablesByLocation(String location, String excludeName) {
+        return collectionMapper.findForageablesByLocation(location, excludeName);
+    }
+
+    // === 키워드 검색 ===
+    public List<FishCollection> searchFish(String keyword) {
+        return collectionMapper.searchFish(keyword);
+    }
+
+    public List<BugCollection> searchBugs(String keyword) {
+        return collectionMapper.searchBugs(keyword);
+    }
+
+    public List<BirdCollection> searchBirds(String keyword) {
+        return collectionMapper.searchBirds(keyword);
+    }
+
+    public List<AnimalCollection> searchAnimals(String keyword) {
+        return collectionMapper.searchAnimals(keyword);
+    }
+
+    public List<CookingCollection> searchCookings(String keyword) {
+        return collectionMapper.searchCookings(keyword);
+    }
+
+    public List<FlowerCollection> searchFlowers(String keyword) {
+        return collectionMapper.searchFlowers(keyword);
+    }
+
+    public List<GardeningCollection> searchCrops(String keyword) {
+        return collectionMapper.searchCrops(keyword);
+    }
+
+    public List<ForageableCollection> searchForageables(String keyword) {
+        return collectionMapper.searchForageables(keyword);
+    }
+
+    // === Count (기존) ===
     public int getFishCount() {
         return collectionMapper.countAllFish();
     }
